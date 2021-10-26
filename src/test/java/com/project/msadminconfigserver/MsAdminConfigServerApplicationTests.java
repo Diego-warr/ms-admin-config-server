df76cce64ea2e69bd18452879747fb56668ed2b8
diegodevrod <diego.rodriguez@avivel.com.pe>
2021-10-25 20:06:31 -0500

    Initial commit

---
+++ src/test/java/com/project/msadminconfigserver/MsAdminConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.msadminconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
